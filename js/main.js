3bd09aabd087524cc78d503ba5ab848da1f3e1a6
Sergii Taran <serg.taran1970@gmail.com>
2026-09-09 12:58:10 +0300

    feat: rebuild faq with native accordion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import { initTheme } from "./modules/theme.js";
 import { initMenu } from "./modules/menu.js";
 import { initProjects } from "./modules/projects.js";
-import { initQuestionsAccordion } from "./modules/questions.js";
 
 import "./modules/reviews.js";
 import "swiper/css";
@@ -9,4 +8,3 @@ import "swiper/css";
 initTheme();
 initMenu();
 initProjects();
-initQuestionsAccordion();
